24278a8eb19f5b1c3a6f3964bb0f13aec800f0fb
ThotaMac <thotamacbookpro16@mymac.local>
2023-04-19 02:40:11 +0800

    setting up client(frontend) with webpack

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Loan System</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
